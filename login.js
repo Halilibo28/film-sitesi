87e9e74df2f53308c9bf3dcb0338cbe4a053b03c
Halil İbrahim Akdoğan <halilibrahimakdogan442@gmail.com>
2023-06-08 11:51:19 +0300

    kayıt

---
+++ login.js
@@ -0,0 +1,14 @@
+var a = document.getElementById("Login");
+var l = document.getElementById("Register");
+var h = document.getElementById("btn");
+function register() {
+    a.style.left = "-400px"
+    l.style.left = "50px"
+    h.style.left = "110px"
+}
+
+function login() {
+    a.style.left = "50px"
+    l.style.left = "450px"
+    h.style.left = "0px"
+}
